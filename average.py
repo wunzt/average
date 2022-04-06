444956cce7da8f9947db19b5f27ba7a748bc6f54
wunzt <wunzt@oregonstate.edu>
2022-04-05 20:40:33 -0700

    Assignment 2.a

---
+++ average.py
@@ -0,0 +1,13 @@
+# Author: Thomas Wunz
+# GitHub username: wunzt
+# Date: 4/5/2022
+# Description: Asks the user to input five numbers and prints the average.
+
+print("Please enter five number.")
+num_1 = float(input())
+num_2 = float(input())
+num_3 = float(input())
+num_4 = float(input())
+num_5 = float(input())
+print("The average of those numbers is: ")
+print((num_1 + num_2 + num_3 + num_4 + num_5) / 5)
